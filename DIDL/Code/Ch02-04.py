fa0ebfa911064c8654f3bc9edaa2d2b7ba127904
Stephen CUI <junocuixuan@163.com>
2023-04-27 21:29:50 +0800

    微积分

--- DIDL/Code/Ch02-04.py
+++ DIDL/Code/Ch02-04.py
@@ -1,14 +1,13 @@
 import numpy as np
 from d2l import torch as d2l
 from matplotlib_inline import backend_inline
-from typing import function
 
 
 def f(x: float) -> float:
-    return 3 * x**x - 4 * x
+    return 3 * x ** 2 - 4 * x
 
 
-def numerical_lim(f: function, x: float, h: float) -> float:
+def numerical_lim(f, x: float, h: float) -> float:
     """计算一个一元函数的在某个点处的导数
 
     Args:
@@ -21,3 +20,10 @@ def numerical_lim(f: function, x: float, h: float) -> float:
 
     """
     return (f(x + h) - f(x - h)) / (2 * h)
+
+
+h = 0.1
+for i in range(5):
+    print('h = {:.3f}, numerical limit = {:5f}'.format(
+        h, numerical_lim(f, 1, h)))
+    h *= .1

3496df7326f7eef991851277b9409f0a41b5f67b
Stephen CUI <junocuixuan@163.com>
2023-05-21 21:14:31 +0800

    Constant tensors and variables

---
+++ DLWP/Codes/Ch03/Constant tensors and variables.py
@@ -0,0 +1,10 @@
+"""
+@Description: Constant tensors and variables
+@Author(s): Stephen CUI
+@LastEditor(s): Stephen CUI
+@CreatedTime: 2023-05-21 16:25:16
+"""
+
+import tensorflow as tf
+x = tf.ones(shape=(2, 1))
+print(x)

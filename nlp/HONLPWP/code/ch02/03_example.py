905ed4edeef8f66321cd5dc19cab0e60d63197ef
Stephen CUI <junocuixuan@163.com>
2023-08-23 22:33:36 +0800

    训练词袋分类器

---
+++ nlp/HONLPWP/code/ch02/03_example.py
@@ -0,0 +1,7 @@
+"""
+@Title: 训练词袋分类器
+@Author(s): Stephen CUI
+@LastEditor(s): Stephen CUI
+@CreatedTime: 2023-08-23 22:32:32
+@Description: 词袋作为文本的向量表示，其中每个向量维都能捕获文本中单词的出现频率、存在与否或加权值，但是不能捕获单词之间的顺序。
+"""
